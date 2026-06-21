349e24a8e36dfe53250fa601fb8bd96652111a47
kenek <kennethesa324@gmail.com>
2026-06-20 20:21:33 -0600

    Fix some error in the path and adding comparations in lowercase

--- Python_Basics/18. PythonJsonManagement_Extra/pythonJsonManagement2_2.py
+++ Python_Basics/18. PythonJsonManagement_Extra/pythonJsonManagement2_2.py
@@ -1,5 +1,5 @@
 import json
-PATH = "../17. PythonJsonManagement1/pokemons.json"
+PATH = "../17. PythonJsonManagement/pokemons.json"
 
 
 def read_json():
@@ -8,12 +8,12 @@ def read_json():
     return loaded_data
 
 
-def find_pokemon_by_name(pokemon_list : list[dict], type_to_find : str):
+def find_pokemon_by_type(pokemon_list : list[dict], type_to_find : str):
     pokemon_names : list[str] = []
     for pokemon in pokemon_list:
         pokemon_type : str = pokemon.get("type")
         pokemon_name : str = pokemon.get("name")
-        if pokemon_type == type_to_find:
+        if pokemon_type.lower() == type_to_find.lower():
             pokemon_names.append(pokemon_name)
     return pokemon_names
 
@@ -21,7 +21,7 @@ def find_pokemon_by_name(pokemon_list : list[dict], type_to_find : str):
 if __name__ == "__main__":
     try:
         type_to_find = input("Please enter the pokemon type to find (Fire, Water, Plant, etc): ")
-        pokemon_names = find_pokemon_by_name(read_json(), type_to_find)
+        pokemon_names = find_pokemon_by_type(read_json(), type_to_find)
         print(f"The pokemons with type '{type_to_find}' are...")
         for pokemon in pokemon_names:
             print(pokemon)
