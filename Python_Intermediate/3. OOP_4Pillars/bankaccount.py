4c09845149b54dc0513f849bb603e80ec799fd60
kenek <kennethesa324@gmail.com>
2026-06-28 12:39:27 -0600

    Arranging the exercises

---
+++ Python_Intermediate/3. OOP_4Pillars/bankaccount.py
@@ -0,0 +1,35 @@
+class BankAccount:
+    balance = 0
+
+    def add_money(self, amount):
+        self.balance += amount
+
+
+    def remove_money(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print("No enough money.")
+
+
+class SavingsAccount(BankAccount):
+    def __init__(self, min_balance):
+        self.min_balance = min_balance
+        self.balance = min_balance
+
+
+    def remove_money(self, amount):
+        if self.balance - self.min_balance > amount:
+            self.balance -= amount
+        else:
+            print(f"The minimum money in the Saving account is: S{self.min_balance}")
+
+
+saves = SavingsAccount(1200)
+print(saves.balance)
+saves.add_money(900)
+print(saves.balance)
+saves.remove_money(1100)
+print(saves.balance)
+
+
